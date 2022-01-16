ef28c3e329c2849ee5a819ffc92a7f99048f2443
dominikhustava <dominikhustava1@gmail.com>
2022-01-16 11:22:48 +0100

    Added FunctionalInterface anotation and some theory

--- src/io/hustava/Greeting.java
+++ src/io/hustava/Greeting.java
@@ -1,5 +1,10 @@
 package io.hustava;
 
+@FunctionalInterface
 public interface Greeting {
     void perform();
 }
+
+//@FunctionalInterface helps us protect lambdas which use interfaces and can be broken by adding another method.
+//We have to be really careful with updating/adding things in interfaces in general, cuz its hard to tell what is using it.
+//from java 8 we can have default method implementation in interfaces
